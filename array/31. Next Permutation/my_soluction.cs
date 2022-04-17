0e92dd31d34590378c868cbffb43f52531cf3884
GlockTian <petertianzhong@gmail.com>
2022-04-17 21:30:53 +1200

    31 with sample

--- array/31. Next Permutation/my_soluction.cs
+++ array/31. Next Permutation/my_soluction.cs
@@ -1,6 +1,37 @@
-public class Solution {
+public class Solution{
     public void NextPermutation(int[] nums) {
-        // can be improved
+        int i = nums.Length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.Length - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+    
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.Length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+
+public class SolutionFirst {
+    public void NextPermutation(int[] nums) {
         int lastAscIndex = -1;// find the first index that is the last ascending part of the int array
         int rightMostIndex = -1;// the index of the value to replace lastAscIndex
         int rightMostValue = 101;
